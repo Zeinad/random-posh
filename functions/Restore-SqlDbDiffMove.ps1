85316e4e5cd47f2d4f113899949e395f56a3ce4e
Daniel Crowson <danieljcrowson@gmail.com>
2017-06-28 12:44:06 -0500

    split out function from script

--- functions/Restore-SqlDbDiffMove.ps1
+++ functions/Restore-SqlDbDiffMove.ps1
@@ -1,13 +1,3 @@
-[string]$RestoreServer='your_RestoreServer'
-[string]$DatabaseName='your_databasename'
-[string]$BackupFileFull='your_filepathandname'
-
-#Optional:
-[string]$BackupFileDiff=''
-[string]$DataFileDirectory=''
-[string]$LogFileDirectory=''
-
-
     function Restore-SqlDbDiffMove {
     <#
 
@@ -139,16 +129,3 @@
             Restore-SqlDatabase -ServerInstance $RestoreServer -Database $DatabaseName -BackupFile "$BackupFileDiff" -ConnectionTimeout 0
         }
     }
-
-
-$parms = @{
-    'RestoreServer'=$RestoreServer;
-    'DatabaseName'=$DatabaseName;
-    'BackupFileFull'=$BackupFileFull;
-    'BackupFileDiff'=$BackupFileDiff;
-    'DataFileDirectory'=$DataFileDirectory;
-    'LogFileDirectory'=$LogFileDirectory;
-    'Verbose'=$True;
-}
-
-Restore-SqlDbDiffMove @parms
---
+++ scripts/basicSqlRestore.ps1
@@ -0,0 +1,25 @@
+#Mandatory:
+[string]$RestoreServer='your_RestoreServer'
+[string]$DatabaseName='your_databasename'
+[string]$BackupFileFull='your_filepathandname'
+
+#Optional:
+[string]$BackupFileDiff=''
+[string]$DataFileDirectory=''
+[string]$LogFileDirectory=''
+
+
+$ErrorActionPreference = "Stop";
+. "$((get-item $PSScriptRoot).Parent.FullName)\functions\Restore-SqlDbDiffMove.ps1"
+
+$parms = @{
+    'RestoreServer'=$RestoreServer;
+    'DatabaseName'=$DatabaseName;
+    'BackupFileFull'=$BackupFileFull;
+    'BackupFileDiff'=$BackupFileDiff;
+    'DataFileDirectory'=$DataFileDirectory;
+    'LogFileDirectory'=$LogFileDirectory;
+    'Verbose'=$True;
+}
+
+Restore-SqlDbDiffMove @parms
